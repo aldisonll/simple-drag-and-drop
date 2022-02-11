920c0422d87dd4893d491629c4690035057a8830
Aldison Lluka <34491203+aldisonll@users.noreply.github.com>
2022-02-11 21:55:42 +0100

    Create manifest.js

---
+++ manifest.js
@@ -0,0 +1,18 @@
+{
+  "id": "PLUGIN-ID",
+  "name": "Drag And Drop",
+  "host": [
+    {
+      "app": "XD",
+      "minVersion": "26.0.0"
+    }
+  ],
+  "version": "1.0.0",
+  "uiEntryPoints": [
+    {
+      "type": "panel",
+      "label": "Drag And Drop",
+      "panelId": "UI"
+    }
+  ]
+}
